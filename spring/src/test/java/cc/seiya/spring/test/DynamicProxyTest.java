b862e6ede40015d034a494395d9f9e3ed122802f
lexihc <lexihc@163.com>
2018-01-29 18:12:05 +0800

    添加aop源码 阅读

--- spring/src/test/java/cc/seiya/spring/test/DynamicProxyTest.java
+++ spring/src/test/java/cc/seiya/spring/test/DynamicProxyTest.java
@@ -1,8 +1,10 @@
 package cc.seiya.spring.test;
 
+import cc.seiya.spring.aop.proxy.CglibProxy;
 import cc.seiya.spring.aop.proxy.JdkTaobaoProxy;
 import cc.seiya.spring.aop.proxy.OnlineShop;
 import cc.seiya.spring.aop.proxy.TeaOnlineShop;
+import cc.seiya.spring.aop.proxy.WineOnlineShop;
 import java.lang.reflect.Proxy;
 import org.junit.Test;
 
@@ -10,14 +12,21 @@ import org.junit.Test;
  * @author: libo
  * @date: 2018/1/29 15:20
  */
-public class JdkDynamicProxyTest {
+public class DynamicProxyTest {
 
     @Test
-    public void doTest() {
+    public void jdkDynamicProxyTest() {
         TeaOnlineShop teaShop = new TeaOnlineShop();
         OnlineShop o = (OnlineShop) Proxy
                 .newProxyInstance(OnlineShop.class.getClassLoader(), new Class[]{OnlineShop.class},
                         new JdkTaobaoProxy(teaShop));
         o.sell(100);
+    }
+
+    @Test
+    public void cglibDynamicProxyTest(){
+        CglibProxy cglibTaobaoProxy = new CglibProxy();
+        WineOnlineShop wineOnlineShop = cglibTaobaoProxy.getProxy(WineOnlineShop.class);
+        wineOnlineShop.sell(10);
     }
 }
